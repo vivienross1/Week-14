5ef9b2630b04081873f1933d3c86753f66c9e36f
vivienross1 <145514057+vivienross1@users.noreply.github.com>
2024-01-05 16:18:01 -0800

    Add files via upload

---
+++ pet-store/src/main/java/pet/store/controller/error/GlobalErrorHandler.java
@@ -0,0 +1,28 @@
+package pet.store.controller.error;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalErrorHandler {
+
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	public Map<String, String> handleNoSuchElementException(NoSuchElementException ex) {
+		log.info("NoSuchElementException occurred: {}", ex.getMessage());
+		
+		Map<String, String> errorResponse = new HashMap<>();
+		errorResponse.put("message", ex.toString());
+		return errorResponse;
+	}
+}
